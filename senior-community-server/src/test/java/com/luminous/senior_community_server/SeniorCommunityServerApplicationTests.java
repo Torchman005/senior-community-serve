b9fc8b41af2de733b40d480cb28c2636440a4192
Torchman005 <2318257072@qq.com>
2025-09-06 10:44:28 +0800

    后端

---
+++ senior-community-server/src/test/java/com/luminous/senior_community_server/SeniorCommunityServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luminous.senior_community_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeniorCommunityServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
